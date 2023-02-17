93ff01492dc23f2997c89fb3446f0e8bcb344453
shrubom <shruti.panicker21@gmail.com>
2023-02-17 14:39:59 +0200

    commit day 2/2

--- sandbox.js
+++ sandbox.js
@@ -9,3 +9,21 @@ var age = 33;
 var isBlackBelt = false;
 var nameString = "Shruti";
 //nameString = 44;
+//SInce we are not defining any kind of type to diameter, we can basically pass anything and it wont give us any error
+var circ1 = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ1("Hi"));
+//SO the above way is an issue, So in Typescript we can say it is number and then use it
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
+//Typescript allows to check and clean the code before it is being developed and shipped.
+//But JS does not do that.
+//Arrays and Objects
+var names = ["John", "DOe"];
+names.push("Mary");
+//names.push(10);
+//When we declare a new array then the types of its elememts cannot be changed at a later stage.
+//We can use a mixed arrays bt we need to declare it before we can do that.
