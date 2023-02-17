34105c9a8c46c8474e6e7d489d56223f4612e121
shrubom <shruti.panicker21@gmail.com>
2023-02-17 14:07:51 +0200

    commit day 2

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+var character = "shruti";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (inputs) {
+    console.log(inputs);
+});
+var age = 33;
+var isBlackBelt = false;
+var nameString = "Shruti";
+//nameString = 44;
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+const character = "shruti";
+console.log(character);
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach((inputs) => {
+  console.log(inputs);
+});
+
+let age = 33;
+let isBlackBelt = false;
+let nameString = "Shruti";
+
+//nameString = 44;
